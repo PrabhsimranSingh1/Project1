b413e462f22001ac0c459ccd89886f4a6c3d9ce0
Prabhsimran Singh <40806595@live.napier.ac.uk>
2026-02-25 23:48:36 +0000

    feat: connect to mysql and run sample querysetup complete

--- ReportApp/Program.cs
+++ ReportApp/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//fefwef
+﻿using MySql.Data.MySqlClient;
+
+using System;
+// Use ONE of these depending on what you installed:
+// using MySql.Data.MySqlClient;
+
+
+class Program
+{
+    static void Main()
+    {
+        // CHANGE Password= to your real root password
+        var connectionString = "Server=localhost;Database=world;User=root;Password=prabhjohal04197@;Port=3306;";
+
+        try
+        {
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("Connected to MySQL ✅");
+
+            var query = "SELECT ID, Name, CountryCode, District, Population FROM city LIMIT 5;";
+            using var cmd = new MySqlCommand(query, connection);
+            using var reader = cmd.ExecuteReader();
+
+            Console.WriteLine("ID | Name | CountryCode | District | Population");
+            Console.WriteLine("------------------------------------------------");
+            while (reader.Read())
+            {
+                Console.WriteLine(
+                    $"{reader["ID"]} | {reader["Name"]} | {reader["CountryCode"]} | {reader["District"]} | {reader["Population"]}"
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed ❌");
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine("Press ENTER to exit...");
+        Console.ReadLine();
+    }
+}
